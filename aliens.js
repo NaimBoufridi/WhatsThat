d4d0ace33ffcc141c99ba5994a023a125cff2bd7
mar45690 <martin.kim94@yahoo.com>
2018-10-07 16:56:29 -0400

    my message

--- aliens.js
+++ aliens.js
@@ -1,5 +1,5 @@
-const number = 2012
+const number= 5 
 const string = 'mayans were right 2012=2018' 
 for (let i = 0; i < number; i++) {
-  console.log(string)
+  console.log('naim stop being toxici')
 }
